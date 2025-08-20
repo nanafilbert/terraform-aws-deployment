7227820b33c78914f81d57a60073ea3a78e1b546
Nana Faty <filbertnanablessing@gmail.com>
2025-08-20 14:11:31 +0000

    prod and dev all set

--- environment/prod/main.tf
+++ environment/prod/main.tf
@@ -1,17 +1,166 @@
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "filbert-tf-state-bucket"
+module "vpc" {
+  source = "git::https://github.com/nanafilbert/terraform_aws_modules_repo.git//terraform_aws_module/modules/vpc?ref=v1.1.0" 
+  name                   = var.name
+ vpc_cidr                = var.vpc_cidr
+  public_subnet_cidrs    = var.public_subnet_cidrs
+  private_subnet_cidrs   = var.private_subnet_cidrs
+  azs                    = var.azs
+  eip_vpc                = var.eip_vpc
+  tags                   = var.tags
+  enable_dns_support     = var.enable_dns_support
+  enable_dns_hostnames   = var.enable_dns_hostnames
+}
+
+module "security_grps" {
+  source = "git::https://github.com/nanafilbert/terraform_aws_modules_repo.git//terraform_aws_module/modules/security_grps?ref=v1.1.0"
+   vpc_id        = module.vpc.vpc_id
+  https_port    = var.https_port
+  protocol      = var.protocol
+  db_port       = var.db_port
+  Admin_CIDR    = var.Admin_CIDR
+  http_port     = var.http_port
+  ssh_port      = var.ssh_port
+  protocol_2    = var.protocol_2
+  outbound_port = var.outbound_port
+  tags          = var.tags
+  name = var.name
+
+
+  
+}
+
+
+
+module "ec2" {
+  source = "git::https://github.com/nanafilbert/terraform_aws_modules_repo.git//terraform_aws_module/modules/ec2?ref=v1.1.0"
+  name                       = var.name
+  instances                  = var.instances
+  key_name                   = var.key_name
+  subnet_id                  = module.vpc.public_subnet_ids[0]  # pick one subnet
+  security_group_ids         = [module.security_grps.app_sg_id] # must be list
+  associate_public_ip_address = var.associate_public_ip_address
+  user_data                  = var.user_data
+  tags                       = var.tags
+}
+
+
+resource "aws_ssm_parameter" "db_password" {
+  name        = "/${var.name}/app/db_password"
+  description = "Database password for the application"
+  type        = "SecureString"
+  value       = var.db_password
+  
+}
+resource "aws_ssm_parameter" "db_username" {
+  name        = "/${var.name}/app/db_username"
+  description = "Database username for the application"
+  type        = "String"
+  value       = var.db_username
+  
+}
+
+resource "aws_db_subnet_group" "db_subnets" {
+  name       = "${var.name}-app-db-subnet-group"
+  subnet_ids = module.vpc.private_subnet_ids
+  tags       = merge(var.tags, { Name = "${var.name}-db-subnet-group" })
+  
+}
+
+resource "aws_db_instance" "mydb" {
+  identifier          = "${var.name}-db"
+  allocated_storage    = 10
+
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = var.db_username
+  password             = aws_ssm_parameter.db_password.value
+  db_subnet_group_name = aws_db_subnet_group.db_subnets.name
+  vpc_security_group_ids = [module.security_grps.db_sg_id]
+  publicly_accessible  = false
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+}
+
+
+
+# Application Load Balancer
+resource "aws_lb" "app_alb" {
+  name               = "${var.name}-app-alb"
+  load_balancer_type = "application"
+  security_groups    = [module.security_grps.alb_sg_id]
+  subnets            = module.vpc.public_subnet_ids  
 
   tags = {
-    Name        = "filbert-tf-state-bucket"
-    Project = "DevOps"
+    Environment = var.name
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "my_bucket" {
-  bucket = aws_s3_bucket.my_bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+# Target Group
+resource "aws_lb_target_group" "app_tg" {
+  name     = "${var.name}-app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 2
+    matcher             = "200-399"
+  }
 }
+
+# Listener (route HTTP → Target Group)
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
+# Attach EC2 Instances to Target Group
+resource "aws_lb_target_group_attachment" "app_instances" {
+  for_each =  module.ec2.filbert_output 
+
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = each.value
+  port             =80
+}
+
+
+
+
+terraform {
+  backend "s3" {
+    bucket = "filbert-tf-state-bucket"
+    key = "devops/infrastructure/s3.tfstate"
+    region = "us-east-2"
+     use_lockfile = true
+     encrypt      = true
+   
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.8.0"
+    }
+  }
+  required_version = ">= 1.12.2"
+}
+
+
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+
---
+++ environment/prod/terraform.tfvars
@@ -0,0 +1,49 @@
+name                     = "filbert"
+eip_vpc            = true
+enable_dns_support = true
+enable_dns_hostnames = true
+vpc_cidr                 = "10.0.0.0/16"
+azs                      = ["us-east-2a", "us-east-2b"]
+public_subnet_cidrs      = ["10.0.1.0/24", "10.0.2.0/24"]
+private_subnet_cidrs     = ["10.0.3.0/24", "10.0.4.0/24"]
+db_port = 3306
+http_port      = 80
+https_port          = 443
+protocol            = "tcp"
+protocol_2          = "-1"
+ssh_port            = 22
+outbound_port = 0
+outbound_cidr_blocks = ["0.0.0.0/0"]
+key_name = "one"
+instances = {
+  webserver = "t2.micro"
+  appserver = "t2.small"
+}
+user_data = <<-EOT
+#!/bin/bash
+apt-get update -y
+apt-get upgrade -y
+apt-get install -y nginx
+systemctl enable nginx
+systemctl start nginx
+echo "<h1>Hello from NGINX on $(hostname -f)</h1>" > /var/www/html/index.nginx-debian.html
+EOT
+
+db_username = "admin"
+
+tags = {
+  environment = "development"
+  project     = "terraform_aws_module_assignment"
+  member1     = "Emmanuel Buatsie-Detse"
+  member2     = "James Agbenu"
+  member3     = "Suzzette Naomi Sappor"
+  member4     = "Godfred Boateng"
+  member5     = "Emmanuel Atwam"
+  member6     = "Maxwell Tyron"
+  member7     = "Filbert Nana Blessing"
+}
+
+
+
+
+#
